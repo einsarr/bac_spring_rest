208b5bc0de0fc01463f91165a65a6f934b768a8b
Moussa SARR <einsarr@gmail.com>
2021-08-30 19:52:20 +0000

    crud rest annee

--- src/main/java/com/isi/dao/IAnnee.java
+++ src/main/java/com/isi/dao/IAnnee.java
@@ -1,10 +1,15 @@
 package com.isi.dao;
 
 import com.isi.entities.Annee;
+import com.isi.entities.Etudiant;
 import com.isi.entities.EtudiantExamen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAnnee extends JpaRepository<Annee,Long> {
+    @Query("SELECT annee FROM Annee annee WHERE annee.libelle_anne =:lib")
+    public Annee findByLibelle_anne(@Param("lib")int lib);
 }
--- src/main/java/com/isi/services/AnneeController.java
+++ src/main/java/com/isi/services/AnneeController.java
@@ -2,9 +2,7 @@ package com.isi.services;
 
 import com.isi.dao.IAnnee;
 import com.isi.entities.Annee;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,28 +12,49 @@ public class AnneeController {
     public AnneeController(IAnnee anneeDao){
         this.anneeDao = anneeDao;
     }
-    @RequestMapping(value = "/annees",method = RequestMethod.GET)
-    public List<Annee> getAll(){
-        return anneeDao.findAll();
-    }
-/*
-    @RequestMapping(value = "/annees",method = RequestMethod.GET)
-    public Annee getAnnee(){
-        return anneeDao.findAll();
-    }
 
     @RequestMapping(value = "/annees",method = RequestMethod.GET)
     public List<Annee> getAll(){
         return anneeDao.findAll();
     }
 
-    @RequestMapping(value = "/annees",method = RequestMethod.GET)
-    public List<Annee> getAll(){
-        return anneeDao.findAll();
+    @RequestMapping(value = "/annees/{id}",method = RequestMethod.GET)
+    public Annee getAnnee(@PathVariable Long id){
+        return anneeDao.findById(id).get();
     }
 
-    @RequestMapping(value = "/annees",method = RequestMethod.GET)
-    public List<Annee> getAll(){
+    @RequestMapping(value = "/getAnnee",method = RequestMethod.GET)
+    public Annee getAnneeByLib(@RequestParam int lib){
+        return anneeDao.findByLibelle_anne(lib);
+    }
+
+    @RequestMapping(value="/annees/{id}",method = RequestMethod.PUT)
+    public Annee updateAnnee(@RequestBody Annee annee, @PathVariable Long id) {
+
+        return anneeDao.findById(id)
+                .map(an -> {
+                    an.setLibelle_anne(annee.getLibelle_anne());
+                    return anneeDao.save(an);
+                })
+                .orElseGet(() -> {
+                    annee.setId(id);
+                    return anneeDao.save(annee);
+                });
+    }
+    @PostMapping("/annees")
+    public Annee saveAnnee(@RequestBody Annee annee) {
+        return anneeDao.save(annee);
+    }
+
+    @DeleteMapping("/annee/{id}")
+    public int deleteAnnees(@PathVariable Long id) {
+         anneeDao.deleteById(id);
+         return 1;
+    }
+
+    @RequestMapping(value="/annees/{id}",method = RequestMethod.DELETE)
+    public List<Annee> deleteAnnee(@PathVariable Long id) {
+        if(id!=null)  anneeDao.deleteById(id);
         return anneeDao.findAll();
-    }*/
+    }
 }
